7c5d12deef4c4e8f7f25101aac5378b2fbe83c90
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-07-07 17:29:54 +0000

    Intitial commit

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<< endl;
+    cout<<"Priyanshu"<< endl;
+    cout<<"Gupta" << endl;
+    int age;
+    cin>>age;
+    cout<<age;
+    return 0;
+}
